168db5087d9074f23ba609115a50507330899e4d
utareyu <uta5315@gmail.com>
2022-06-02 17:55:50 +0900

    -

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 fetch('https://utareyu.github.io/shirocom/main.json')
   .then(response => response.json())
   .then(function(x){
-		
-		for(i=0;i<(Object.keys(x["data"][1]["cont"]).length);i++){
+		const dsel=1;
+		for(i in Object.keys(x["data"][dsel]["cont"])){
+			let d = x["data"][dsel]["cont"][i]["data"];
 			const p = document.createElement('p');
-			let d = x["data"][1]["cont"][i]["data"];
-			let a = document.createElement('a');
-			let aTagv = "";
+			const a = document.createElement('a');
+			const br = document.createElement('br');
+			const div = document.createElement('div')
+			const dest = document.getElementById("hrp");
 
 			// --- a処理
+			let aTagv = "";
 			let aTag=d.match(/http.*/g,'\n');
 			if(aTag!=null){
 				aTag=aTag[0].replace(/".*/g,'');
@@ -17,15 +20,31 @@ fetch('https://utareyu.github.io/shirocom/main.json')
 				}
 				a.setAttribute("href",aTag);
 				a.textContent=aTagv;
-				document.getElementById("hrp").appendChild(a);
+				dest.appendChild(a);
 				// console.log(aTag);
 				continue;
 			}
 
+			// --- br処理
+			if(d.match("<br>")){
+				let work;
+				d = d.split('<br>');
+				console.log(d);
+				for(i in d){
+					p.textContent=i+": "+d[i];
+					p.id= i;
+					console.log(d[i]);
+					console.log(p);
+					div.appendChild(p);
+				}
+				div.id = "data";
+				dest.appendChild(div);
+			continue;
+			}
+
 			// --- data出力
-			d=d.replace(/<br>/g,'\n');
 			p.textContent = d;
-			document.getElementById("hrp").appendChild(p);
+			dest.appendChild(p);
 
 		}
 			console.log(Object.keys(x["data"][1]["cont"]).length);
