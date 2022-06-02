ee3090dd7f584a8867430f3748bd9811a18ea3ac
utareyu <uta5315@gmail.com>
2022-06-02 22:52:13 +0900

    -

--- test.js
+++ test.js
@@ -3,20 +3,39 @@
 //   ■   ■■■■■  ■■■    ■
 //   ■   ■         ■   ■
 //   ■   ■■■■■  ■■■    ■
+
 fetch('https://utareyu.github.io/shirocom/main.json')
   .then(response => response.json())
   .then(function(x){
+		const mem =[];
+		for(i in Object.keys(x["member"])){
+			mem[i] = x["member"][i]["name"];
+		}
+		console.log(mem);
 		const dsel=1;
+
+		for(u in Object.keys(x["data"][dsel])){
+			// console.log("-"+u);
+			for(y in Object.keys(x["data"][dsel]["cont"])){
+				// console.log("--"+y);
+			}
+		}
+
+
 		for(i in Object.keys(x["data"][dsel]["cont"])){
-
-
 			// ##### data処理
 			let d = x["data"][dsel]["cont"][i]["data"];
-			const dest = document.getElementById("hrp");
+			const dest = document.getElementById("dest");
 			const p = document.createElement('p');
+			const m = x["data"][dsel]["cont"][i]["member"];
 			const y=x["data"][dsel]["cont"][i]["data"];
 
-			console.log(typeof(y) +"'"+(y.charAt(1)=='a'?"lnk":"")+(y.substr(0,3)=="<img"?"img":"")+"' : "+ y);
+			// console.log(mem[m]);
+			p.innerHTML=mem[m];
+			p.className="member";
+			dest.insertAdjacentElement("beforeend",p);
+
+			// console.log(typeof(y) +"'"+(y.charAt(1)=='a'?"lnk":"")+(y.substr(0,3)=="<img"?"img":"")+"' : "+ y);
 			if(y.charAt(0)=='<'){
 				dest.insertAdjacentHTML("beforeend",y);
 				// # a処理
@@ -30,7 +49,8 @@ fetch('https://utareyu.github.io/shirocom/main.json')
 				}
 			} else {
 				p.innerHTML=y;
-				dest.appendChild(p);
+				p.className="data";
+				dest.insertAdjacentElement("beforeend",p);
 			}
 			// #####
 		}
