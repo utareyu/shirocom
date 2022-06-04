47c218d771e2f1c437a342d93028f12d1cc0d8b3
utareyu <uta5315@gmail.com>
2022-06-04 11:51:12 +0900

    v1.1

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ function run(x){
 				
 				dest.textContent=x["data"][dsel]["title"]+" ";
 
-				wk="<span style=\"font-size:50%;font-weight:400;\">"+da+"</span>";
-				console.log(dest);
+				wk=`<span style="font-size:50%;font-weight:400;">${da}</span>`;
+				console.log(da);
 				dest.insertAdjacentHTML("beforeend",wk);
 
 				dest = document.getElementById("nav");
@@ -27,23 +27,19 @@ function run(x){
 				if(t.length>10){
 					t=t.substr(0,9)+"…";
 				}
-				wk="<p onclick=\""+
-				"run("+i+")"+
-				"\">"+t+"</p>";
-				console.log(dest);
-				dest.insertAdjacentHTML("beforeend",wk);
-
-
+				wk=`<p onclick="run(${i})">${t}</p>`;
 				console.log(t);
+				dest.insertAdjacentHTML("beforeend",wk);
 			}
 
 			const mem =[[],[]];
-			for(i in Object.keys(x["member"]))mem[0][i] = x["member"][i]["name"];
-			for(i in Object.keys(x["member"]))mem[1][i] ="m"+ i+".ico";
+			for(i in Object.keys(x["member"])) mem[0][i] = x["member"][i]["name"];
+			for(i in Object.keys(x["member"])) mem[1][i] =`m${i}.png`;
 			console.log(mem);
 
+			console.log(`%c---- ${dsel} ----`,'color: yellow;font-size: 2em;');
+
 			for(i in Object.keys(x["data"][dsel]["cont"])){
-				console.log(i);
 				
 				d = x["data"][dsel]["cont"][i]["data"];
 				dest = document.getElementById("main");
@@ -62,44 +58,46 @@ function run(x){
 				
 				//div .ico
 				dest=document.getElementById("p"+i);
-				// wk="<div class=\"container ico\"><img class=\"ico\" src=\"https://mcbeeringi.github.io/shirokepu/img/m"+m+".png\"></div>";
-				wk="<div class=\"container ico\"><img class=\"ico\" src=\"ico\\m"+m+".png\"></div>";
-				console.log(dest);
+				wk=`<div class="container ico"><img class="ico" src="ico\\${mem[1][m]}"></div>`;
+				// console.log(mem[1][m]);
 				dest.insertAdjacentHTML("beforeend",wk);
 
 				//div .content
-				wk="<div class=\"container content\"></div>";
-				console.log(dest);
+				wk=`<div class="container content"></div>`;
+				// console.log(wk);
 				dest.insertAdjacentHTML("beforeend",wk);
 
 				//div .member
 				dest=dest.lastElementChild;
-				wk="<div class=\"member\">"+mem[0][m]+"</div>";
-				console.log(dest);
+				wk=`<div class="member">${mem[0][m]}</div>`;
+				console.log(`-- ${mem[0][m]}`);
 				dest.insertAdjacentHTML("beforeend",wk);
 
 				//div .data
 				dest=document.getElementById("p"+i).lastElementChild;
-				wk="<div class=\"data\"></div>";
-				console.log(dest);
+				wk=`<div class="data"></div>`;
+				// console.log(wk);
 				dest.insertAdjacentHTML("beforeend",wk);
 
 				dest=document.getElementById("p"+i).getElementsByClassName("data")[0];
-				d=d.replace(/<.?script.*?>/g,'!すくりぷと!');
+				d=d.replace(/<.?script.*?>/g,'::');
 				if(d.charAt(0)=='<'){
 					dest.insertAdjacentHTML("beforeend",d);
 					// # a処理
 					if(d.charAt(1)=='a'){
 						const a = dest.lastElementChild;
-						let ahref=d.match(/http.*/g,'\n').join().replace(/".*/g,'');
-						if(ahref.length>=40){
-							ahref=ahref.substr(0,26)+"…"+ahref.substr(ahref.length-20);
+						let wk=d.match(/http.*/g,'\n').join().replace(/".*/g,'');
+						console.log(wk);
+						if(wk.length>=40){
+							wk=`${wk.substr(0,wk.indexOf("/", 8))}…${
+								wk.substr(wk.lastIndexOf("/")).length>=25 ? wk.substr(wk.length-25) : wk.substr(wk.lastIndexOf("/"))
+							}`;
 						}
-						a.textContent=ahref;
+						a.textContent=wk;
 					}
 				} else {
 					p.innerHTML=d;
-					console.log(dest);
+					console.log(d);
 					dest.insertAdjacentElement("beforeend",p);
 				}
 				
@@ -107,7 +105,7 @@ function run(x){
 
 					//div.act
 					dest=document.getElementById("p"+i).getElementsByClassName("content")[0];
-					wk="<div class=\"container act\"></div>";
+					wk=`<div class="container act"></div>`;
 					dest.insertAdjacentHTML("beforeend",wk);
 
 					for(j in Object.keys(x["data"][dsel]["cont"][i]["act"])){
@@ -117,22 +115,22 @@ function run(x){
 
 						//div .s
 						dest=document.getElementById("p"+i).getElementsByClassName("act")[0];
-						wk="<div  id=\"a"+i+j+"\"class=\"s\"></div>";
-						console.log(dest);
+						wk=`<div  id=\"a${i}${j}\"class=\"s\"></div>`;
+						console.log(e+" "+c);
 						dest.insertAdjacentHTML("beforeend",wk);
 						dest.lastElementChild.textContent=e+" "+c;
 
 						//span .balloon
 						dest=dest.lastElementChild;
 						wk="<span class=\"balloon\"></span>";
-						console.log(dest);
+						// console.log(dest);
 						dest.insertAdjacentHTML("beforeend",wk);
 						dest.lastElementChild.textContent=c+"人が反応しました";
 
 						//span "emoji"
 						dest=dest.lastElementChild;
-						wk="<span style=\"font-size: 60px;display: block;\"></span>";
-						console.log(dest);
+						wk=`<span style="font-size: 60px;display: block;"></span>`;
+						// console.log(dest);
 						dest.insertAdjacentHTML("afterbegin",wk);
 						dest.lastElementChild.textContent=e;
 					}
